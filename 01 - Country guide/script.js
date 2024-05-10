6bdae4e869e9901a17c0fa5d4e3689c8c618d257
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2024-05-10 21:41:05 +0300

    fiz bugs

--- 01 - Country guide/script.js
+++ 01 - Country guide/script.js
@@ -1,11 +1,11 @@
-function name(params) {
-    let searchBtn = document.getElementById("search-btn");
-    let countryInp = document.getElementById("country-inp");
-    searchBtn.addEventListener("click", () => {
-      let countryName = countryInp.value;
-      let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-      console.log(finalURL);
-      fetch(finalURL)
+// function name(params) {
+//     let searchBtn = document.getElementById("search-btn");
+//     let countryInp = document.getElementById("country-inp");
+//     searchBtn.addEventListener("click", () => {
+//       let countryName = countryInp.value;
+//       let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+//       console.log(finalURL);
+//       fetch(finalURL)
         .then((response) => response.json())
         .then((data) => {
         
