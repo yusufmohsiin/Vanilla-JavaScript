162e75bbb5461d89bef03441500052e6df7f53eb
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2024-05-15 21:56:36 +0300

    Project 09

--- 09 - Drag me/script.js
+++ 09 - Drag me/script.js
@@ -1,72 +1,74 @@
-let draggableElem = document.getElementById("draggable-elem");
-let initialX = 0,
-  initialY = 0;
-let moveElement = false;
+newFunction();
+
+function newFunction() {
+    let draggableElem = document.getElementById("draggable-elem");
+    let initialX = 0, initialY = 0;
+    let moveElement = false;
 
 
-let events = {
-  mouse: {
-    down: "mousedown",
-    move: "mousemove",
-    up: "mouseup",
-  },
-  touch: {
-    down: "touchstart",
-    move: "touchmove",
-    up: "touchend",
-  },
-};
+    let events = {
+        mouse: {
+            down: "mousedown",
+            move: "mousemove",
+            up: "mouseup",
+        },
+        touch: {
+            down: "touchstart",
+            move: "touchmove",
+            up: "touchend",
+        },
+    };
 
 
-let deviceType = "";
+    let deviceType = "";
 
-const isTouchDevice = () => {
-  try {
-    document.createEvent("TouchEvent");
-    deviceType = "touch";
-    return true;
-  } catch (e) {
-    deviceType = "mouse";
-    return false;
-  }
-};
+    const isTouchDevice = () => {
+        try {
+            document.createEvent("TouchEvent");
+            deviceType = "touch";
+            return true;
+        } catch (e) {
+            deviceType = "mouse";
+            return false;
+        }
+    };
 
-isTouchDevice();
+    isTouchDevice();
 
 
-draggableElem.addEventListener(events[deviceType].down, (e) => {
-  e.preventDefault();
-  initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
-  initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+    draggableElem.addEventListener(events[deviceType].down, (e) => {
+        e.preventDefault();
+        initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+        initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
 
-  moveElement = true;
-});
+        moveElement = true;
+    });
 
-//Move
-draggableElem.addEventListener(events[deviceType].move, (e) => {
-  
-  if (moveElement) {
-    e.preventDefault();
-    let newX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
-    let newY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
-    draggableElem.style.top =
-      draggableElem.offsetTop - (initialY - newY) + "px";
-    draggableElem.style.left =
-      draggableElem.offsetLeft - (initialX - newX) + "px";
-    initialX = newX;
-    initialY = newY;
-  }
-});
+    draggableElem.addEventListener(events[deviceType].move, (e) => {
 
-//mouse up / touch end
-draggableElem.addEventListener(
-  events[deviceType].up,
-  (stopMovement = (e) => {
-    moveElement = false;
-  })
-);
+        if (moveElement) {
+            e.preventDefault();
+            let newX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+            let newY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+            draggableElem.style.top =
+                draggableElem.offsetTop - (initialY - newY) + "px";
+            draggableElem.style.left =
+                draggableElem.offsetLeft - (initialX - newX) + "px";
+            initialX = newX;
+            initialY = newY;
+        }
+    });
 
-draggableElem.addEventListener("mouseleave", stopMovement);
-draggableElem.addEventListener(events[deviceType].up, (e) => {
-  moveElement = false;
-});
+
+    draggableElem.addEventListener(
+        events[deviceType].up,
+        (stopMovement = (e) => {
+            moveElement = false;
+        })
+    );
+
+    draggableElem.addEventListener("mouseleave", stopMovement);
+    draggableElem.addEventListener(events[deviceType].up, (e) => {
+        moveElement = false;
+    });
+}
