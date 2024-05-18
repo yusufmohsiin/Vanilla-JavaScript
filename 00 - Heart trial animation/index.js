bd38bf5144211220e4575a193d9ed604fc757e15
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2024-05-19 00:03:32 +0300

    logic refactor

--- 00 - Heart trial animation/index.js
+++ 00 - Heart trial animation/index.js
@@ -1,16 +1,20 @@
-const bodyEl = document.querySelector("body");
+newFunction();
 
-bodyEl.addEventListener("mousemove", (event) => {
-  const xPos = event.offsetX;
-  const yPos = event.offsetY;
-  const spanEl = document.createElement("span");
-  spanEl.style.left = xPos + "px";
-  spanEl.style.top = yPos + "px";
-  const size = Math.random() * 100;
-  spanEl.style.width = size + "px";
-  spanEl.style.height = size + "px";
-  bodyEl.appendChild(spanEl);
-  setTimeout(() => {
-    spanEl.remove();
-  }, 3000);
-});
+function newFunction() {
+  const bodyEl = document.querySelector("body");
+
+  bodyEl.addEventListener("mousemove", (event) => {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px";
+    bodyEl.appendChild(spanEl);
+    setTimeout(() => {
+      spanEl.remove();
+    }, 3000);
+  });
+}
