b72accba07cb389fbe3ccb1288d1912d27e3c1bf
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2024-05-21 23:47:00 +0300

    Pro 10 - logical increamental

--- 10 - Calender/script.js
+++ 10 - Calender/script.js
@@ -1,41 +1,45 @@
-const monthEl = document.querySelector(".date h1");
-const fullDateEl = document.querySelector(".date p");
-const daysEl = document.querySelector(".days");
+newFunction();
 
-const monthInx = new Date().getMonth();
-const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
-const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
+function newFunction() {
+    const monthEl = document.querySelector(".date h1");
+    const fullDateEl = document.querySelector(".date p");
+    const daysEl = document.querySelector(".days");
+
+    const monthInx = new Date().getMonth();
+    const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
+    const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
 
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 
-monthEl.innerText = months[monthInx];
-fullDateEl.innerText = new Date().toDateString();
+    monthEl.innerText = months[monthInx];
+    fullDateEl.innerText = new Date().toDateString();
 
-let days = "";
+    let days = "";
 
-for (let i = firstDay; i > 0; i--) {
-  days += `<div class="empty"></div>`;
+    for (let i = firstDay; i > 0; i--) {
+        days += `<div class="empty"></div>`;
+    }
+    for (let i = 1; i <= lastDay; i++) {
+        if (i === new Date().getDate()) {
+            days += `<div class="today">${i}</div>`;
+        } else {
+            days += `<div>${i}</div>`;
+        }
+    }
+
+    daysEl.innerHTML = days;
 }
-for (let i = 1; i <= lastDay; i++) {
-  if (i === new Date().getDate()) {
-    days += `<div class="today">${i}</div>`;
-  } else {
-    days += `<div>${i}</div>`;
-  }
-}
-
-daysEl.innerHTML = days;
