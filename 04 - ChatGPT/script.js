f1f61d08c6449a79ed2cc3b2ae851c618485ace7
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2024-05-27 13:24:53 +0300

    testing...

--- 04 - ChatGPT/script.js
+++ 04 - ChatGPT/script.js
@@ -1,13 +1,3 @@
 document.getElementById("submit-btn").addEventListener("click", function () {
     sendToChatGPT();
   });
-  
-
-  function sendToChatGPT() {
-    let value = document.getElementById("word-input").value;
-  
-    let body = {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: value }],
-      tempreture: "1",
-    };
